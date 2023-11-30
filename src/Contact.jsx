1d03775ba3f1dcd806bc6f5e0a0b84694a9b3cda
ArcticCod <Kokkoalex18@gmail.com>
2023-11-29 18:49:04 -0800

    style form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -29,80 +29,53 @@ export default function ContactScreen() {
       .catch((error) => setSuccess("false"));
   };
   return (
-    <div>
-      <h1 className="text-offwhite font-bold text-[32px] text-center mb-[16px]">
-        Contact Us
-      </h1>
+    <div id={"about"} className="contact">
+      <div className="form-title">contact me:</div>
       {success === "pending" ? (
         <form
+          className="contact-form"
           onSubmit={handleSubmit}
-          className="w-full max-w-[600px] flex flex-col gap-[8px] mx-auto bg-darkaccent p-[32px] rounded-[4px] items-center shadow-small"
           data-netlify="true"
         >
           <input type="hidden" name="form-name" value="contact" />
-          <div className="flex flex-col gap-[4px] w-full">
-            <label
-              htmlFor="name"
-              className="text-offwhite text-[12px] font-bold uppercase leading-[1]"
-            >
-              Name
-            </label>
+          <div className="inputs">
+            <label htmlFor="name">Name</label>
             <input
               id="name"
               name="name"
-              className="w-full focus:outline-none bg-darkness text-offwhite rounded-[4px] py-[2px] px-[8px]"
               type="text"
               required
               value={formData.name}
               onChange={handleChange}
             />
           </div>
-          <div className="flex flex-col gap-[4px] w-full">
-            <label
-              htmlFor="email"
-              className="text-offwhite text-[12px] font-bold uppercase leading-[1]"
-            >
-              Email
-            </label>
+          <div className="inputs">
+            <label htmlFor="email">Email</label>
             <input
               id="email"
               name="email"
-              className="w-full focus:outline-none bg-darkness text-offwhite rounded-[4px] py-[2px] px-[8px]"
               type="email"
               required
               value={formData.email}
               onChange={handleChange}
             />
           </div>
-          <div className="flex flex-col gap-[4px] w-full">
-            <label
-              htmlFor="reason"
-              className="text-offwhite text-[12px] font-bold uppercase leading-[1]"
-            >
-              Message
-            </label>
+          <div className="inputs">
+            <label htmlFor="reason">Message</label>
             <textarea
               id="message"
               name="message"
-              className="w-full h-[200px] focus:outline-none bg-darkness text-offwhite rounded-[4px] py-[2px] px-[8px] resize-none"
               required
               value={formData.message}
               onChange={handleChange}
             ></textarea>
           </div>
-          <button className="py-[4px] px-[16px] rounded-[4px] bg-accent text-darkness font-bold uppercase">
-            Submit
-          </button>
+          <button className="submit-button">Submit</button>
         </form>
       ) : success === "true" ? (
-        <p className="p-[16px] bg-darkaccent rounded-[4px] shadow-small text-[24px] text-offwhite text-center max-w-[350px] mx-auto">
-          Thank you for reaching out! A member of our staff will be in contact
-          with you shortly.
-        </p>
+        <p>Thank you for reaching out!</p>
       ) : (
-        <p className="p-[16px] bg-darkaccent rounded-[4px] shadow-small text-[24px] text-offwhite text-center max-w-[300px] mx-auto">
-          Form submission failed, please refresh and try again.
-        </p>
+        <p>Form submission failed, please refresh and try again.</p>
       )}
     </div>
   );
